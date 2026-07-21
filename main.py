ee5d40816ae7ff231aa51499c470fe356dde653a
Phrosyne <mcoderej@gmail.com>
2026-07-20 20:23:44 -0700

    accurately parsing and sending array  to website

--- main.py
+++ main.py
@@ -13,71 +13,29 @@ scan_buffer = deque(maxlen=3) #automatic memory management
 data_lock = Lock()
 running = False
 has_new_data = False
-scan_count = 0
 
 config = vd.Config()
 config.timestamp_first_packet = True
 config.min_range = 5.0
 config.max_range = 100.0
                         
-def streamAll(port, data):
-    if data:        
-        print(f"Streaming from PCAP: {data}")
-        try:
-            for stamp, points in vd.read_pcap(pcap_file=data, config=config):
-                if not running:
-                    break
-                scan_count += 1
-                with data_lock:
-                    scan_buffer.append(points)
-                    total_points = sum(len(s) for s in scan_buffer)
-                    has_new_data = True
-                # time.sleep(0.1)
-        except Exception as e:
-            print(f"PCAP error: {e}")
-    elif port:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 26214400) #25mb
-        
-        try:
-            sock.bind(('', port))
-            sock.settimeout(0.1)
-            print(f"Socket bound on UDP port {port}")
-            
-            stream_decoder = vd.StreamDecoder(config)
-            print("velodyne_decoder StreamDecoder initialized")
-            
-            packet_count = 0
-            
-            try:
-                data, addr = sock.recvfrom(2000)
-                packet_count += 1
-                
-                current_time = time.time()
-                result = stream_decoder.decode(current_time, data, False)
-                
-                if result is not None:
-                    points = result.second if hasattr(result, 'second') else result[1]
-                    if points is not None and len(points) > 0:
-                        scan_count += 1
-                        with data_lock:
-                            scan_buffer.append(points)
-                            total_points = sum(len(s) for s in scan_buffer)
-                            has_new_data = True
-
-            except Exception as e:
-                if "TimePair" not in str(e):
-                    print(f"\nUDP error: {e}")
-        except Exception as e:
-            print(f"Socket error: {e}")
-        finally:
-            sock.close()
-            print("\nSocket closed")
-
+def streamAll():
+    global running
+    global has_new_data
+    print("Streaming from PCAP")
+    try:
+        for stamp, points in vd.read_pcap(pcap_file='./data.pcap', config=config):
+            if not running:
+                break                
+            with data_lock:
+                scan_buffer.append(points)
+                total_points = sum(len(s) for s in scan_buffer)
+                has_new_data = True
+            time.sleep(0.1)
+    except Exception as e:
+        print(f"PCAP error: {e}")
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -91,14 +49,32 @@ async def endpoint(websocket: WebSocket):
     global has_new_data
     await websocket.accept()
     print("Client connected")
-    while True:
-        await websocket.send_text('world')
-        await asyncio.sleep(5)
+    try:
+        while True:
+            if has_new_data:
+                print('reached newdata')
+                with data_lock:
+                    print('reached datalock')
+                    array = np.concatenate(list(scan_buffer)) if len(scan_buffer) > 1 else scan_buffer[0]
+                    has_new_data = False
+                    points = points = np.ascontiguousarray(array[:, :3], dtype=np.float32)
+                    
+                    xyz = points.tobytes()
+                    await websocket.send_bytes(xyz)
+            await asyncio.sleep(0.1)
+    except Exception as e:
+        print(f"Error: {e}")
+                
+def start():
+    global running
+    running = True
+    t = Thread(target=streamAll, daemon=True)    
+    t.start()
+    print("started streaming thread")
+    
     
 def main():
-    t = Thread(target=streamAll(port=None, data='./data.pcap'), daemon=True)
-    t.start()
-    
+    start()
     uvicorn.run(app=app, host="0.0.0.0", port=8000)
     
     
